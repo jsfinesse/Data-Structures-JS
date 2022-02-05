9fc9e3500aba487268f812596ff75222e1a4d54e
Prithu Singh <prithusingh0327@gmail.com>
2022-02-05 15:21:06 +0530

    Add Singly Linked List class

---
+++ Linked Lists/Singly_Lindked_List.js
@@ -0,0 +1,17 @@
+// piece of data - val
+// reference to next node - next
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Data-Structures-JS
+
+Implementation of Data Structures and related algorithms in JavaScript
