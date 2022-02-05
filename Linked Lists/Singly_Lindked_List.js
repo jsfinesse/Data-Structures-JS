a57e80bc3c1ab192d0462a2f54681a3c657d047a
Prithu Singh <prithusingh0327@gmail.com>
2022-02-05 16:34:50 +0530

    Add push and pop

--- Linked Lists/Singly_Lindked_List.js
+++ Linked Lists/Singly_Lindked_List.js
@@ -14,4 +14,36 @@ class SinglyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+    push(val) {
+        const newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        else {
+            // set the next property of the current tail to the newly created node
+            this.tail.next = newNode;
+            // update the new tail to be the newly created node
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    pop() {
+        if(!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while(current.next) {
+            newTail = current;
+            current = currnet.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
 }
