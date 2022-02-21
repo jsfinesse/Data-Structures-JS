17ea94899c275507362db31f7d87fb29c9606c3f
Prithu Singh <prithusingh0327@gmail.com>
2022-02-22 01:17:55 +0530

    Added shift

--- Linked Lists/Doubly_Linked_Lists.js
+++ Linked Lists/Doubly_Linked_Lists.js
@@ -14,11 +14,10 @@ class DoublyLinkedList {
     }
     push(val) {
         const newNode = new Node(val);
-        if(this.length === 0) {
+        if (this.length === 0) {
             this.head = newNode;
             this.tail = this.head;
-        }
-        else {
+        } else {
             this.tail.next = newNode;
             newNode.prev = this.tail;
             this.tail = newNode;
@@ -27,18 +26,33 @@ class DoublyLinkedList {
         return this;
     }
     pop() {
-        if(!this.head) {
+        if (!this.head) {
             return undefined;
         }
         const removed = this.tail;
         if (this.length === 1) {
             this.head = null;
             this.tail = null;
-        }
-        else {
+        } else {
             this.tail = removed.prev;
             this.tail.next = null;
             removed.prev = null;
+        }
+        this.length--;
+        return removed;
+    }
+    shift() {
+        if (this.length === 0) {
+            return undefined;
+        }
+        const removed = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = removed.next;
+            this.head.prev = null;
+            removed.next = null;
         }
         this.length--;
         return removed;
