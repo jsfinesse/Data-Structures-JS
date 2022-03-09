5c838e4f437c28676faa70ade72ae5714c1cb5d5
Prithu Singh <prithusingh0327@gmail.com>
2022-03-10 00:25:10 +0530

    Added BFS

--- BSTs/Binary_Search_Tree.js
+++ BSTs/Binary_Search_Tree.js
@@ -55,4 +55,21 @@ class BinarySearchTree {
         }
         return false;
     }
+    BreadthFirstSearch() {
+        let data = [], queue = [];
+        let node = this.root;
+
+        queue.push(node);
+        while(queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if(node.left) {
+                queue.push(node.left);
+            }
+            if(node.right) {
+                queue.push(node.right);
+            }
+            return data;
+        }
+    }
 }
