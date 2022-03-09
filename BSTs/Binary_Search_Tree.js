82f5426ffcd0c8c6eefdc2e675414bd8ae9fcce5
Prithu Singh <prithusingh0327@gmail.com>
2022-03-09 22:25:13 +0530

    Added insert

--- BSTs/Binary_Search_Tree.js
+++ BSTs/Binary_Search_Tree.js
@@ -10,4 +10,33 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+    insert(val) {
+        const newNode = new Node(val);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while (true) {
+                if (val === current.value) {
+                    return undefined;
+                }
+                if (val < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (val > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
 }
