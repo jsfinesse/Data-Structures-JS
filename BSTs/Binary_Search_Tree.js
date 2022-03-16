5e59037f46aba607422d631f377ed47459119f38
Prithu Singh <prithusingh0327@gmail.com>
2022-03-16 18:09:33 +0530

    Added DFS PostOrder and InOrder

--- BSTs/Binary_Search_Tree.js
+++ BSTs/Binary_Search_Tree.js
@@ -103,4 +103,60 @@ class BinarySearchTree {
 
         return data;
     }
+    DFSPostOrder() {
+        /* 
+                10
+            <       >
+            6       15
+        <       >       >
+        3       8       20
+        */
+
+        // Output = [3, 8, 6, 20, 15, 10]
+
+        let data = [];
+        let current = this.root;
+
+        function traverse(node) {
+            if(node.left) {
+                traverse(node.left);
+            }
+
+            if(node.right) {
+                traverse(node.right);
+            }
+
+            data.push(node.value);
+        }
+
+        traverse(current);
+
+        return data;
+    }
+    DFSInOrder() {
+        /* 
+                10
+            <       >
+            6       15
+        <       >       >
+        3       8       20
+        */
+
+        // Output = [3, 6, 8, 10, 15, 20]
+        
+        let data = [];
+        let current = this.root;
+
+        function traverse(node) {
+            if(node.left) {
+                traverse(node.left);
+            }
+            
+            data.push(node.value);
+            
+            if(node.right) {
+                traverse(node.right);
+            }
+        }
+    }
 }
