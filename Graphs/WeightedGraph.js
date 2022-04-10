35c75b6eaa42f9cac81539e1a955e78cde6702f8
Prithu Singh <prithusingh0327@gmail.com>
2022-04-10 13:21:35 +0530

    Added Weighted Graph class

---
+++ Graphs/WeightedGraph.js
@@ -0,0 +1,14 @@
+class WeightedGraph {
+    constructor() {
+        this.adjacenyList = {};
+    }
+    addVertex(vertex) {
+        if (!this.adjacenyList[vertex]) {
+            this.adjacenyList[vertex] = [];
+        }
+    }
+    addEdge(vertex1, vertex2, weight) {
+        this.adjacenyList[vertex1].push({ node: vertex2, weight });
+        this.adjacenyList[vertex2].push({ node: vertex1, weight });
+    }
+}
